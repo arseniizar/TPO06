203139c44b85c214b9548083c1b606c9e2ca3bd1
arseniizar <arseniizarudniuk@gmail.com>
2025-04-14 22:37:42 +0200

    initial commit

---
+++ src/test/java/com/example/tpo06/Tpo06ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tpo06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tpo06ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
